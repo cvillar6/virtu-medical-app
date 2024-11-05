b0980d78759cb7c5607025e7f6ba500df1ee031e
cvilla-lealco <cvilla@leal.co>
2024-11-04 20:40:28 -0500

    Footer desktop was added

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { BreakpointObserverService } from '../../services/breakpoint-observer.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {
+export class FooterComponent implements OnDestroy {
+  isDesktop: boolean = false;
 
+  private _breakpointObserverSubscription: Subscription =
+    this.breakpointObserverService.observeBreakpoints().subscribe(result => {
+      result.breakpoints['(min-width: 1024px)']
+        ? (this.isDesktop = true)
+        : (this.isDesktop = false);
+    });
+
+  constructor(private breakpointObserverService: BreakpointObserverService) {}
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+import { ContactUsFormComponent } from './components/contact-us-form/contact-us-form.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
-  imports: [CommonModule, MatExpansionModule, MatIconModule, RouterModule],
+  declarations: [HeaderComponent, FooterComponent, ContactUsFormComponent],
+  imports: [
+    CommonModule,
+    MatExpansionModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    RouterModule,
+  ],
   exports: [HeaderComponent, FooterComponent],
 })
 export class SharedModule {}

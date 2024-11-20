86b9552a93b9a94cb292e7c7a8a4f32814548f0f
cvilla-lealco <cvilla@leal.co>
2024-11-20 18:00:04 -0500

    Links added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -26,6 +26,10 @@ const routes: Routes = [
     loadChildren: () =>
       import('./contact-us/contact-us.module').then(m => m.ContactUsModule),
   },
+  {
+    path: '**',
+    redirectTo: '',
+  },
 ];
 
 @NgModule({

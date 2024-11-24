3e94cbb81d9aa073d772e0d12baeea09d6a471bb
cvilla-lealco <cvilla@leal.co>
2024-11-24 15:50:07 -0500

    Resources

--- src/app/contact-us/components/contact-us-form/contact-us-form.component.ts
+++ src/app/contact-us/components/contact-us-form/contact-us-form.component.ts
@@ -19,4 +19,12 @@ export class ContactUsFormComponent {
   onSubmit(): void {
     throw new Error('Method not implemented.');
   }
+
+  recipient = 'example@example.com'; // Replace with the actual recipient's email
+  subject = 'Your Subject Here'; // Replace with the email subject
+  body = 'Hello, this is the body of the email.'; // Replace with the email body
+
+  get emailHref(): string {
+    return `mailto:${this.recipient}?subject=${encodeURIComponent(this.subject)}&body=${encodeURIComponent(this.body)}`;
+  }
 }
